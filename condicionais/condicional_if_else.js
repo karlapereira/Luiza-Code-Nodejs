cfb429dd5c0400062f3442c1ece0503e2cb6eae9
karlapereira <karlapereira@gec.inatel.br>
2022-04-03 11:51:37 -0300

    exemplos condicionais javascript

---
+++ condicionais/condicional_if_else.js
@@ -0,0 +1,9 @@
+let semaforo = 'vermelho';
+
+if(semaforo === 'verde'){
+    console.log("Pode passar!")
+}else if(semaforo === 'vermelho'){ 
+    console.log("Esperar!")
+}else{
+    console.log("Tomar cuidado!")
+}
---
+++ condicionais/condicional_if_else_2.js
@@ -0,0 +1,8 @@
+let senha = "admin123";
+let senhaCorreta = "admin123";
+
+if(senha === senhaCorreta){
+    console.log("Senha correta!")
+}else{
+    console.log("Senha incorreta!")
+}
