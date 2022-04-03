509ced321c2cb1ace84ab5f6814c02ee70bdb16b
karlapereira <karlapereira@gec.inatel.br>
2022-04-03 12:09:58 -0300

    exemplo_swith_case

---
+++ condicionais/condicional_switch_case.js
@@ -0,0 +1,16 @@
+let menu = 4;
+let mensagem = "";
+
+switch(menu){
+    case(1):
+        mensagem = "Pizza com borda de queijo!"
+        break
+    case(2):
+        mensagem = "Pizza com borda de catupiry!"
+        break
+    default:
+        mensagem = "Pizza sem borda!"
+        break
+}
+
+console.log(mensagem)
