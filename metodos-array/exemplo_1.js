1e770e6ccb4f29e8fa040eedda7b214fc4405c9a
karlapereira <karlapereira@gec.inatel.br>
2022-04-04 22:05:08 -0300

    adionando exemplo1 metodos-array

--- metodos-array/exemplo_1.js
+++ metodos-array/exemplo_1.js
@@ -1,25 +1,26 @@
 let listaNumeros = [1, 2, 3, 4, 5]
+let indexElemento = 0 
 
 //length - tamanho da lista
 console.log(listaNumeros.length)
 
-//pop - remover um item no final da nossa lista
-console.log("Removendo: "+listaNumeros.pop())
+//pop() - remover um item no final da nossa lista
+console.log(listaNumeros.pop())
 
-//push - adicionar um elemento no fim da nossa lista
-console.log(listaNumeros.push(6))
+//push() - adicionar um elemento no fim da nossa lista
+listaNumeros.push(6)
 
-//shift - remove um elemento no inicio da lista
+//shift() - remove um elemento no inicio da lista
 console.log(listaNumeros.shift())
 
-//unshift - adicionar um elemento no inicio da lista
-console.log(listaNumeros.unshift(0))
+//unshift() - adicionar um elemento no inicio da lista
+listaNumeros.unshift(0)
 
-//indexOf - retorna o indice do elemento da nossa lista
-let indexElemento = listaNumeros.indexOf(2)
+//indexOf() - retorna o indice do elemento da nossa lista
+indexElemento = listaNumeros.indexOf(4)
+//console.log(indexElemento)
 
-//splice - remove 1 item pela posição
+//splice(pos, quantidade) - remove 1 item pela posição 
 listaNumeros.splice(indexElemento, 2)
-
 
 console.log("Lista: "+listaNumeros)
